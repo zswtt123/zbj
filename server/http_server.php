8c3ba62696d91ff32c98bce1d4b58c006eedf218
root <root@test.sw.com>
2019-06-01 22:11:27 +0800

    v1

---
+++ server/http_server.php
@@ -0,0 +1,34 @@
+<?php
+
+$http = new swoole_http_server("0.0.0.0",8811);
+
+$http->set(
+          [
+            'enable_static_handler' =>true,
+            'document_root'  =>  "/swoole_live/thinkphp/public/static",
+            'worker_num'=>5,
+           ]
+
+);
+
+$http->on('request',function($request,$response){
+      $response->cookie('singma','xssss',time()+1800);
+     $response->end("sss".json_encode($request->get));
+
+});
+
+
+$http->start();
+
+
+
+
+
+
+
+
+
+
+
+
+?>
