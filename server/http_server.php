556a5645074cbe12f25594f9fa01e1053700de59
zswtt123 <3434467011@qq.com>
2019-06-02 21:42:10 +0800

    v5.31

--- server/http_server.php
+++ server/http_server.php
@@ -56,7 +56,7 @@ $http->on('request',function($request,$response) use($http){
 
       }
 
-//ob_start();
+ob_start();
       // 执行应用并响应
       try{
 think\Container::get('app', [APP_PATH])
@@ -66,9 +66,9 @@ think\Container::get('app', [APP_PATH])
        //todo
 }
 //echo "-action-".request()->action().PHP_EOL;
-//$res = ob_get_contents();
-//ob_end_clean();
-$response->end();
+$res = ob_get_contents();
+ob_end_clean();
+$response->end($res);
 //$http->close();
 });
 
