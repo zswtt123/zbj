4892294eada9da4308a017c665ddd8898b82b978
zswtt123 <3434467011@qq.com>
2019-06-02 22:15:18 +0800

    v5.23

--- thinkphp/library/think/App.php
+++ thinkphp/library/think/App.php
@@ -372,7 +372,7 @@ class App implements \ArrayAccess
      */
     public function routeCheck()
     {
-        $path = $this->request->path();
+        $path = $this->request->path();echo $path;
         $depr = $this->config('app.pathinfo_depr');
 
         // 路由检测
