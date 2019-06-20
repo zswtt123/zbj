6d04c1a4b5b44f1aa37f01b9f55501d2ece32e8a
zswtt123 <3434467011@qq.com>
2019-06-20 19:11:53 +0800

    223

--- server/http_server.php
+++ server/http_server.php
@@ -16,7 +16,7 @@ $http->on('WorkerStart',function(swoole_server $server, $worker_id){
 // define('APP_PATH', __DIR__ . '/../application/');
 
 // require __DIR__ . '/../thinkphp/base.php';
-// });
+});
 
 
 $http->on('request',function($request,$response) use($http){
